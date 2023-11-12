8260ab04384a95950a56c7fc53a290a1a52e714d
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-11-12 15:24:17 +0100

    added photos for featured work section + added js code to effect on photos when we press on the buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,21 @@ $(function () {
         upperNavHeight = $(".upper-bar").innerHeight(),
         navHeight = $(".navbar").innerHeight()
     $(".slider, .carousel-item").height(windowHeight - (upperNavHeight + navHeight));
+
+
+    // featured work shuffle
+
+    $(".featured-work ul li").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        if($(this).data("class") === "all") {
+            $(".shuffle-imgs .col-md").css("opacity", "1");
+        }else {
+            $(".shuffle-imgs .col-md").css("opacity", ".08");
+            $($(this).data("class")).parent().css("opacity", "1");
+        }
+    })
+
+
 });
+
+
